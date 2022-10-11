be5d184d411a2d879fa61d7a7f6ba4709c04b62b
USER <ajitwable5@gmail.com>
2022-10-11 22:57:41 +0530

    Merge remote-tracking branch 'origin/master' database

--- app/src/main/java/com/example/rental/SetPassword.java
+++ app/src/main/java/com/example/rental/SetPassword.java
@@ -3,12 +3,40 @@ package com.example.rental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.common.data.DataHolder;
 
 public class SetPassword extends AppCompatActivity {
+    EditText hintName,hintEmail,hintMobileNO,hintUserId,hintPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_password);
+    }
+
+    public void Process(View view) {
+        hintName=(EditText) findViewById(R.id.hintName);
+        hintEmail=(EditText) findViewById(R.id.hintEmail);
+        hintMobileNO=(EditText) findViewById(R.id.hintMobileNo);
+        hintUserId=(EditText) findViewById(R.id.hintUserId);
+        hintPassword=(EditText) findViewById(R.id.hintPassword);
+
+        String name=hintName.getText().toString().trim();
+        String email= hintEmail.getText().toString().trim();
+        String mobileNo=hintMobileNO.getText().toString().trim();
+        String userId = hintUserId.getText().toString().trim();
+        String password = hintPassword.getText().toString().trim();
+
+
+
+
+
+
+
+
+
     }
 }
